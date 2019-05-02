0f09d05c8495f8ca18b967af53ef367d7814241d
unknown <bgleoquip@gmail.com>
2019-05-03 00:18:21 +0530

    form started

--- src/components/Header/BasicHeader.js
+++ src/components/Header/BasicHeader.js
@@ -2,37 +2,31 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 export default class BasicHeader extends Component {
-    render () {
-        return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <Link className="navbar-brand">Leo Super Admin</Link>
-            <div className="collapse navbar-collapse">
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/dashboard">
-                            Dashboard
-                        </Link>
-                    </li>
-                </ul>
-                <ui className="nav navbar-nav ml-auto">
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/Signin">
-                            Sign In
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/Signup">
-                            Sign Up
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/Signout">
-                            Sign Out
-                        </Link>
-                    </li>
-                </ui>
-            </div>
-            </nav>
-        )
-    }
-} 
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{ marginBottom: '30px' }}>
+        <Link className="navbar-brand" to="/">Leo oauth</Link>
+
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+
+          <ul className="nav navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/signup">Sign Up</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/signin">Sign In</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/signout">Sign Out</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { createStore } from 'redux';
+import {Provider} from 'react-redux';
 
-import * as serviceWorker from './serviceWorker';
+
+import registerServiceWorker  from './serviceWorker';
 
 import App from './components/App';
 import Home from './components/Home';
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
 import DashBoard from './components/Dashboard';
+import reducres from './reducers';
 
 ReactDOM.render(
+    <Provider store={createStore(reducres,{})}>
     <BrowserRouter> 
         <App >
             <Route exact path="/" component={Home} />
@@ -19,9 +24,10 @@ ReactDOM.render(
             <Route exact path="/dashboard" component={DashBoard} />
         </App>
     </BrowserRouter>
+    </Provider>
 , document.querySelector('#root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+registerServiceWorker();
